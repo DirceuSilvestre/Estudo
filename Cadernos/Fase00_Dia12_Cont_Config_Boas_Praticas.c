f90eccc81886d36fc06d031c3fb8690894167f39
Dirceu Silvestre <dirceu_silvestre@hotmail.com>
2026-01-21 18:47:51 -0300

    Fase00 Dia12 Cont Config Boas Praticas

---
+++ Cadernos/Fase00_Dia12_Cont_Config_Boas_Praticas.c
@@ -0,0 +1,60 @@
+/* ==============================================================
+   Fase00 — DIA 12 — Continuação da Configuração das Ferramentas e Boas Práticas
+   ==============================================================
+
+   🧩 CONTEÚDOS PRINCIPAIS:
+   - Git
+   - Docker
+   - [Tópico 3]
+
+   🧠 CONCEITOS FUNDAMENTAIS:
+   - Instalar o Git, e conectar ele ao ambiente remoto para sincroniza-los
+   - Instalar o Docker, e buscar correções ou melhorias em vídeos
+   - Tudo ocorreu de forma tranquila, os tutoriais foram suficientes
+
+   --------------------------------------------------------------
+   GIT
+   --------------------------------------------------------------
+
+   INSTALAÇÃO
+   → Fácil e simples, somente clicando nos nexts necessários
+   → Não havia tutorial mais aprofundado para explicar cada página que clicamos next
+   → Funcionamento se dá abrindo o git bash dentro da pasta desejada
+
+   AMBIENTE
+   → Deve ser aberto o git bash na pasta que contem todos os arquivos do projeto
+   → Tem o ambiente da máquina e o ambiente virtual onde o projeto está, no github
+
+   --------------------------------------------------------------
+   DOCKER
+   --------------------------------------------------------------
+
+   INSTALAÇÃO
+   → Modo habitual, poucos comandos de atualização
+   → Dificuldade achar um bom vídeo explicativo, muito curtos ou muito grandes
+
+
+   - Próximo estudo: 1.1 Organização de projetos (expansão de template)
+
+
+   🧱 ERROS E SOLUÇÕES:
+   - Nenhum erro detectado, tudo ocorreu de forma clara e adequada
+
+   🗂️ REFERÊNCIAS CONSULTADAS:
+   - Instalação Docker Completo com WSL2 https://www.youtube.com/watch?v=HXOherCKhyI
+
+   ==============================================================
+   COMMIT PADRÃO:
+      FaseXX_DiaXX_nome_do_tema 
+
+   --------------------------------------------------------------
+
+   💻 EXEMPLO / CÓDIGO PRÁTICO:
+   ============================================================== */
+
+#include <stdio.h>
+
+int main() {
+    printf("Oláaaaaa, mundo!\n");
+    return 0;
+}
