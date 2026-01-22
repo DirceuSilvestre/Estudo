32780b76dbb75e5a68b870473cb52d5ad8ea2467
Dirceu Silvestre <dirceu_silvestre@hotmail.com>
2026-01-21 21:52:38 -0300

    Fase01 Dia 12 Organizacao Projetos

---
+++ Cadernos/Fase01_Dia12_Organizacao_Projetos.c
@@ -0,0 +1,61 @@
+/* ==============================================================
+   Fase01 — DIA 12 — Organização de Projetos
+   ==============================================================
+
+   🧩 CONTEÚDOS PRINCIPAIS:
+   - Porque organizar um projeto
+   - Como organizar um projeto
+   - [Tópico 3]
+
+   🧠 CONCEITOS FUNDAMENTAIS:
+   - Organizar cada arquivo por sua função dentro da pasta correspondente
+   - Nívelar as pastas pelo tipo do conteúdo
+   - Liste insights importantes
+
+   --------------------------------------------------------------
+   PROJETO
+   --------------------------------------------------------------
+
+   TEMPLATE MÍNIMO
+   → Estrutura viável mínima, com src, gitignore, readme
+   → src é o source code, código fonte
+   → gitignore é uma lista de arquivos para serem ignorados no push
+   → readme é uma apresentação organizada do projeto
+
+   SRC - SOURCE CODE
+   → Código, documentação, configuração, artefatos
+   → Artefatos são requisitos, diagramas, executáveis, bibliotecas, imagem docker
+   
+   GIT IGNORE
+   → Tudo que não é código, nem histórico, nem conhecimento
+   → Não subir o que é gerado quando o código roda, como bins e exes
+
+   READ ME
+   → Objetivo é responder, o que é? por que existe? quais ferramentas usa?
+   → Explicação simples, clara e objetiva sobre o projeto
+
+
+   - Próximo estudo: Expansão de Template
+
+
+   🧱 ERROS E SOLUÇÕES:
+   - (Liste erros encontrados e como resolveu)
+
+   🗂️ REFERÊNCIAS CONSULTADAS:
+   - (links, vídeos ou livros usados)
+
+   ==============================================================
+   COMMIT PADRÃO:
+      FaseXX_DiaXX_nome_do_tema 
+
+   --------------------------------------------------------------
+
+   💻 EXEMPLO / CÓDIGO PRÁTICO:
+   ============================================================== */
+
+#include <stdio.h>
+
+int main() {
+    printf("Olá, mundo!\n");
+    return 0;
+}
